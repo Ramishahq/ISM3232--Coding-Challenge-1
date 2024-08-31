e16ac85484deaa06a5ada2a584b9bd9f43faeb14
ramishahq <hoque2@usf.edu>
2024-08-31 14:41:28 -0400

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,10 @@
-//This is my first js file
+// Task1: Variables and Data Types
+
+
+
+let employeeName = "Rami";
+const employeeID = 321; 
+var isActive = true; 
+console.log (employeeName, typeof(employeeName));
+console.log (employeeID, typeof(employeeID));
+console.log (isActive, typeof(isActive));
